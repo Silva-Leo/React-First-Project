fa241567d381bc68f8d9a5f416f59b7b576971b4
Leo Silva <leonardo.ghost@gmail.com>
2022-01-25 18:57:12 -0300

    First Commit

---
+++ first-project/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+//exercicio
+
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+const App = () => {
+  const dados = luana;
+
+  return(
+    <>
+      
+    </>
+  )
+}
+
+export default App;
